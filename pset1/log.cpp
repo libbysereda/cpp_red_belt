db0d68af69d6cdc31c970432bd836927495ae89f
Liubov Sereda <lbvsereda@gmail.com>
2019-01-31 18:55:23 +0300

    Added solution for log.cpp

--- pset1/log.cpp
+++ pset1/log.cpp
@@ -1,3 +1,12 @@
+
+//
+//  log.cpp
+//  red_belt
+//
+//  Created by Liubov Sereda on 30/01/2019.
+//  Copyright © 2019 Liubov Sereda. All rights reserved.
+//
+
 #include "test_runner.h"
 #include <sstream>
 #include <string>
@@ -5,56 +14,76 @@ using namespace std;
 
 class Logger {
 public:
-  explicit Logger(ostream& output_stream) : os(output_stream) {
-  }
+    explicit Logger(ostream& output_stream) : os(output_stream) {
+    }
 
-  void SetLogLine(bool value) { log_line = value; }
-  void SetLogFile(bool value) { log_file= value; }
+    void SetLogLine(bool value) { log_line = value; }
+    void SetLogFile(bool value) { log_file= value; }
 
-  void Log(const string& message);
+    bool GetLogLine() {
+        return log_line;
+    }
+
+    bool GetLogFile() {
+        return log_file;
+    }
+
+    void Log(const string& message) {
+        os << message;
+    }
 
 private:
-  ostream& os;
-  bool log_line = false;
-  bool log_file = false;
+    ostream& os;
+    bool log_line = false;
+    bool log_file = false;
 };
 
-#define LOG(logger, message) ...
+#define LOG(logger, message) {                               \
+  ostringstream os;                                          \
+  if (logger.GetLogFile()) {                                 \
+    os << __FILE__;                                          \
+    if (logger.GetLogLine()) os << ":" << __LINE__;          \
+  } else if (logger.GetLogLine()) os << "Line " << __LINE__; \
+  if (!os.str().empty()) os << " ";                          \
+  os << message << "\n";                                     \
+  logger.Log(os.str());                                      \
+}
 
 void TestLog() {
-/* Для написания юнит-тестов в этой задаче нам нужно фиксировать конкретные
- * номера строк в ожидаемом значении (см. переменную expected ниже). Если
- * мы добавляем какой-то код выше функции TestLog, то эти номера строк меняются,
- * и наш тест начинает падать. Это неудобно.
- *
- * Чтобы этого избежать, мы используем специальный макрос #line
- * (http://en.cppreference.com/w/cpp/preprocessor/line), который позволяет
- * переопределить номер строки, а также имя файла. Благодаря ему, номера
- * строк внутри функции TestLog будут фиксированы независимо от того, какой
- * код мы добавляем перед ней*/
+    /* Для написания юнит-тестов в этой задаче нам нужно фиксировать конкретные
+     * номера строк в ожидаемом значении (см. переменную expected ниже). Если
+     * мы добавляем какой-то код выше функции TestLog, то эти номера строк меняются,
+     * и наш тест начинает падать. Это неудобно.
+     *
+     * Чтобы этого избежать, мы используем специальный макрос #line
+     * (http://en.cppreference.com/w/cpp/preprocessor/line), который позволяет
+     * переопределить номер строки, а также имя файла. Благодаря ему, номера
+     * строк внутри функции TestLog будут фиксированы независимо от того, какой
+     * код мы добавляем перед ней*/
 #line 1 "logger.cpp"
 
-  ostringstream logs;
-  Logger l(logs);
-  LOG(l, "hello");
+    ostringstream logs;
+    Logger l(logs);
+    LOG(l, "hello");
 
-  l.SetLogFile(true);
-  LOG(l, "hello");
+    l.SetLogFile(true);
+    LOG(l, "hello");
 
-  l.SetLogLine(true);
-  LOG(l, "hello");
+    l.SetLogLine(true);
+    LOG(l, "hello");
 
-  l.SetLogFile(false);
-  LOG(l, "hello");
+    l.SetLogFile(false);
+    LOG(l, "hello");
 
-  string expected = "hello\n";
-  expected += "logger.cpp hello\n";
-  expected += "logger.cpp:10 hello\n";
-  expected += "Line 13 hello\n";
-  ASSERT_EQUAL(logs.str(), expected);
+    string expected = "hello\n";
+    expected += "logger.cpp hello\n";
+    expected += "logger.cpp:10 hello\n";
+    expected += "Line 13 hello\n";
+    ASSERT_EQUAL(logs.str(), expected);
 }
 
 int main() {
-  TestRunner tr;
-  RUN_TEST(tr, TestLog);
+    TestRunner tr;
+    RUN_TEST(tr, TestLog);
+
 }
