828019d14070adfc55b105d27e088329c1d45ebe
Liubov Sereda <lbvsereda@gmail.com>
2019-05-16 19:12:52 +0300

    Added stress test

--- pset2/learner.cpp
+++ pset2/learner.cpp
@@ -1,6 +1,12 @@
 #include <algorithm>
 #include <string>
 #include <vector>
+#include <iostream>
+#include <sstream>
+#include <fstream>
+
+#include "test_runner.h"
+#include "profile.h"
 
 using namespace std;
 
@@ -26,3 +32,109 @@ class Learner {
     return dict;
   }
 };
+
+/*
+Learner& createDictionary(Learner& learner) {
+  ifstream infile("words.txt");
+  string line;
+  while(getline(infile, line)) {
+    istringstream iss(line);
+    string word;
+    iss >> word;
+  }
+}
+*/
+
+void getWords(vector<string>& words) {
+  ifstream infile("words_to_test.txt");
+  string line;
+  while (getline(infile, line))
+  {
+    istringstream iss(line);
+    string word;
+    while (iss >> word) {
+      words.push_back(word);
+    }
+  }
+}
+
+void testLearn() {
+  Learner learner;
+
+  vector<string> words_to_learn = {
+    "lake", "new", "world hello", "hello"
+  };
+
+  vector<int> words_counter;
+  string line;
+
+  for (const auto& l : words_to_learn) {
+    vector<string> learned_words;
+    stringstream ss(l);
+    string word;
+    while (ss >> word) {
+      learned_words.push_back(word);
+    }
+    words_counter.push_back(learner.Learn(learned_words));
+  }
+
+  vector<int> expected = {1, 1, 2, 0};
+  ASSERT_EQUAL(words_counter, expected);
+}
+
+void testKnownWords() {
+  Learner learner;
+
+  vector<string> words_to_learn = {
+    "lake", "new", "world hello", "hello"
+  };
+
+  vector<int> words_counter;
+  string line;
+
+  for (const auto& l : words_to_learn) {
+    vector<string> learned_words;
+    stringstream ss(l);
+    string word;
+    while (ss >> word) {
+      learned_words.push_back(word);
+    }
+    words_counter.push_back(learner.Learn(learned_words));
+  }
+
+  vector<string> expected = {"hello", "lake", "new", "world"};
+  ASSERT_EQUAL(learner.KnownWords(), expected);
+}
+
+void stressTest() {
+  vector<string> words_to_learn;
+  getWords(words_to_learn);
+
+  Learner learner;
+  vector<int> words_counter;
+  string line;
+
+  {
+    LOG_DURATION("Learn");
+    for (const auto& l : words_to_learn) {
+      vector<string> learned_words;
+      stringstream ss(l);
+      string word;
+      while (ss >> word) {
+        learned_words.push_back(word);
+      }
+      words_counter.push_back(learner.Learn(learned_words));
+    }
+  }
+  {
+    LOG_DURATION("KnownWords");
+    vector<string> v = learner.KnownWords();
+  }
+}
+
+int main() {
+  TestRunner tr;
+  stressTest();
+  RUN_TEST(tr, testLearn);
+  RUN_TEST(tr, testKnownWords);
+}
