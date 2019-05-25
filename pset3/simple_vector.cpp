8a9d0641811d145575b49d4b93b9b6c9203d9295
Liubov Sereda <lbvsereda@gmail.com>
2019-05-25 19:19:33 +0300

    Working on simple_vector.h

--- pset3/simple_vector.cpp
+++ pset3/simple_vector.cpp
@@ -23,17 +23,79 @@ void TestConstruction() {
   ASSERT_EQUAL(five_strings[2], "Hello");
 }
 
-void TestPushBack() {
-  SimpleVector<int> v;
-  for (int i = 10; i >= 1; --i) {
-    v.PushBack(i);
-    ASSERT(v.Size() <= v.Capacity());
-  }
-  sort(begin(v), end(v));
 
-  const vector<int> expected = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
-  ASSERT_EQUAL(v.Size(), expected.size());
-  ASSERT(equal(begin(v), end(v), begin(expected)));
+void TestPushBack() {
+  {
+    SimpleVector<int> v;
+    vector<int> expected;
+    for (auto& i : v) {
+      expected.push_back(i);
+    }
+
+    ASSERT_EQUAL(v.Size(), expected.size());
+    ASSERT(equal(begin(v), end(v), begin(expected)));
+  }
+  {
+    SimpleVector<int> v;
+    for (int i = 0; i < 1; ++i) {
+      v.PushBack(i);
+    }
+
+    vector<int> expected;
+    for (auto& i : v) {
+      expected.push_back(i);
+    }
+
+    ASSERT_EQUAL(v.Size(), expected.size());
+    ASSERT(equal(begin(v), end(v), begin(expected)));
+    ASSERT_EQUAL(v[0], expected[0]);
+  }
+  {
+    SimpleVector<int> v;
+    for (int i = 0; i < 5; ++i) {
+      v.PushBack(i);
+    }
+
+    vector<int> expected_for;
+    for (auto& i : v) {
+      expected_for.push_back(i);
+    }
+
+    vector<int> expected = {0, 1, 2, 3, 4};
+
+    ASSERT_EQUAL(v.Size(), 5u);
+    ASSERT_EQUAL(expected, expected_for);
+  }
+  /*
+  {
+    SimpleVector<int> v(5);
+    for (int i = 0; i < v.Size(); ++i) {
+      v.PushBack(i);
+    }
+
+    vector<int> expected_for;
+    for (auto& i : v) {
+      expected_for.push_back(i);
+    }
+
+    vector<int> expected = {0, 1, 2, 3, 4};
+
+    ASSERT_EQUAL(v.Size(), 5u);
+    ASSERT_EQUAL(expected, expected_for);
+  } */
+  { /*
+    SimpleVector<int> v;
+    for (int i = 10; i >= 1; --i) {
+      v.PushBack(i);
+      ASSERT(v.Size() <= v.Capacity());
+    }
+    sort(begin(v), end(v));
+
+    const vector<int> expected = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+    ASSERT_EQUAL(v.Size(), expected.size());
+    ASSERT(equal(begin(v), end(v), begin(expected)));
+    */
+  }
 }
 
 int main() {
--- pset3/simple_vector.h
+++ pset3/simple_vector.h
@@ -1,6 +1,8 @@
 #pragma once
 
 #include <cstdlib>
+#include <iostream>
+using namespace std;
 
 // Реализуйте шаблон SimpleVector
 template <typename T>
@@ -15,10 +17,96 @@ public:
   T* begin();
   T* end();
 
+  const T* begin() const;
+  const T* end() const;
+
   size_t Size() const;
   size_t Capacity() const;
   void PushBack(const T& value);
 
 private:
-  // Добавьте поля для хранения данных вектора
+  size_t size_;
+  size_t capacity_;
+  size_t cur_index = 0;
+  T* data_;
+  T* end_;
 };
+
+template <typename T>
+SimpleVector<T>::SimpleVector() {
+  size_ = capacity_ = 0;
+  data_ = end_ = nullptr;
+}
+
+template <typename T>
+SimpleVector<T>::SimpleVector(size_t size) {
+  capacity_ = size_ = size;
+  data_ = new T[size];
+  end_ = data_ + size_;
+}
+
+template <typename T>
+SimpleVector<T>::~SimpleVector() {
+  delete[] data_;
+}
+
+template <typename T>
+T& SimpleVector<T>::operator[](size_t index) {
+  return data_[index];
+}
+
+template <typename T>
+T* SimpleVector<T>::begin() {
+  return data_;
+}
+
+template <typename T>
+T* SimpleVector<T>::end() {
+  return end_;
+}
+
+template <typename T>
+const T* SimpleVector<T>::begin() const {
+  return data_;
+}
+
+template <typename T>
+const T* SimpleVector<T>::end() const {
+  return end_;
+}
+
+template <typename T>
+size_t SimpleVector<T>::Size() const {
+  return size_;
+}
+
+template <typename T>
+size_t SimpleVector<T>::Capacity() const {
+  return capacity_;
+}
+
+template <typename T>
+void SimpleVector<T>::PushBack(const T& value) {
+  if (Size() == 0) {
+    size_ = capacity_ = 1;
+    data_ = new T[size_];
+    end_ = data_ + size_;
+    data_[cur_index++] = value;
+  } else {
+      if (Size() == Capacity()) {
+        T* old_data = data_;
+        capacity_ *= 2;
+        data_ = new T[capacity_];
+
+        for (size_t i = 0; i < Size(); ++i) {
+          data_[i] = old_data[i];
+        }
+
+        delete[] old_data;
+      }
+
+      data_[cur_index++] = value;
+      size_++;
+      end_ = data_ + size_;
+  }
+}
