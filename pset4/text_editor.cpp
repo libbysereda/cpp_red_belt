66148a780737a41e9bf9b4f84f9abd772d5790be
Liubov Sereda <lbvsereda@gmail.com>
2019-06-02 18:46:27 +0300

    Working on text_editor.cpp

--- pset4/text_editor.cpp
+++ pset4/text_editor.cpp
@@ -1,10 +1,14 @@
 #include <string>
+#include <sstream>
+#include <list>
+
 #include "test_runner.h"
+#include "profile.h"
+
 using namespace std;
 
 class Editor {
- public:
-  // Реализуйте конструктор по умолчанию и объявленные методы
+public:
   Editor();
   void Left();
   void Right();
@@ -13,7 +17,65 @@ class Editor {
   void Copy(size_t tokens = 1);
   void Paste();
   string GetText() const;
+
+private:
+  list<char> text;
+  list<char>::iterator cursor_pos = text.begin();
+  list<char> buffer;
 };
+
+Editor::Editor() {}
+
+void Editor::Left() {
+  if (cursor_pos != text.begin()) {
+    --cursor_pos;
+  }
+}
+
+void Editor::Right() {
+  if (cursor_pos != text.end()) {
+    cursor_pos++;
+  }
+}
+
+void Editor::Insert(char token) {
+  text.insert(cursor_pos, token);
+}
+
+void Editor::Cut(size_t tokens) {
+  // try splice
+  buffer.clear();
+  if (!text.empty()) {
+    auto end = cursor_pos;
+    advance(end, tokens);
+    buffer.assign(cursor_pos, end);
+    text.erase(next(cursor_pos), end);
+  }
+}
+
+void Editor::Copy(size_t tokens) {
+  // try splice
+  buffer.clear();
+  if (!text.empty()) {
+    auto end = cursor_pos;
+    advance(end, tokens);
+    buffer.assign(cursor_pos, end);
+  }
+}
+
+void Editor::Paste() {
+
+}
+
+string Editor::GetText() const {
+  ostringstream os;
+  for (auto it = text.begin(); it != text.end(); ++it) {
+    os << *it;
+  }
+  return os.str();
+}
+
+// Unit tests
 
 void TypeText(Editor& editor, const string& text) {
   for(char c : text) {
@@ -104,11 +166,157 @@ void TestEmptyBuffer() {
   ASSERT_EQUAL(editor.GetText(), "example");
 }
 
+void TestDefault() {
+  Editor editor;
+  const string text = "hello, world";
+  for (char c : text) {
+    editor.Insert(c);
+  }
+  // Текущее состояние редактора: `hello, world|`
+  for (size_t i = 0; i < text.size(); ++i) {
+    editor.Left();
+  }
+  // Текущее состояние редактора: `|hello, world`
+  editor.Cut(7);
+  // Текущее состояние редактора: `|world`
+  // в буфере обмена находится текст `hello, `
+  for (size_t i = 0; i < 5; ++i) {
+    editor.Right();
+  }
+  // Текущее состояние редактора: `world|`
+  editor.Insert(',');
+  editor.Insert(' ');
+  // Текущее состояние редактора: `world, |`
+  editor.Paste();
+  // Текущее состояние редактора: `world, hello, |`
+  editor.Left();
+  editor.Left();
+  //Текущее состояние редактора: `world, hello|, `
+  editor.Cut(3); // Будут вырезаны 2 символа
+  // Текущее состояние редактора: `world, hello|`
+  ASSERT_EQUAL(editor.GetText(), string("world, hello"));
+}
+
+string GenerateText() {
+   const int SIZE = 1000000;
+   const int WORD_LENGTH = 1000000000;
+   string text(SIZE, 'a');
+   for (int i = WORD_LENGTH; i < SIZE; i += WORD_LENGTH) {
+     text[i] = ' ';
+   }
+   return text;
+ }
+
+const string TEXT = GenerateText();
+
+void StressTestInsertGet() {
+  Editor editor;
+  {
+    LOG_DURATION("Insert");
+    TypeText(editor, TEXT);
+  }
+  {
+    LOG_DURATION("GetText");
+    editor.GetText();
+  }
+}
+
+void StressTestCutPaste() {
+  Editor editor;
+  TypeText(editor, TEXT);
+  {
+    LOG_DURATION("Cut/Paste 100");
+    for (int i = 0; i < 50000; ++i) {
+      editor.Cut(100);
+      editor.Paste();
+    }
+  }
+
+  Editor editor2;
+  TypeText(editor2, TEXT);
+  {
+    LOG_DURATION("Cut/Paste 1");
+    for (int i = 0; i < 1000000; ++i) {
+      editor2.Cut(1);
+      editor2.Paste();
+    }
+  }
+}
+
+void StressTestCut() {
+  Editor editor;
+  TypeText(editor, TEXT);
+  {
+    LOG_DURATION("Cut 100");
+    for (int i = 0; i < 50000; ++i) {
+      editor.Cut(100);
+    }
+  }
+
+  Editor editor2;
+  TypeText(editor2, TEXT);
+  {
+    LOG_DURATION("Cut 1");
+    for (int i = 0; i < 1000000; ++i) {
+      editor2.Cut(1);
+    }
+  }
+}
+
+void StressTestCopy() {
+  Editor editor;
+  TypeText(editor, TEXT);
+  {
+    LOG_DURATION("Copy 100");
+    for (int i = 0; i < 50000; ++i) {
+      editor.Copy(100);
+    }
+  }
+
+  Editor editor2;
+  TypeText(editor2, TEXT);
+  {
+    LOG_DURATION("Copy 1");
+    for (int i = 0; i < 1000000; ++i) {
+      editor2.Copy(1);
+    }
+  }
+
+}
+
+void StressTestLeftRight() {
+  Editor editor;
+  TypeText(editor, TEXT);
+
+  {
+    LOG_DURATION("Left");
+    for (int i = 0; i < 1000000; ++i) {
+      editor.Left();
+    }
+  }
+
+  {
+    LOG_DURATION("Right");
+    for (int i = 0; i < 1000000; ++i) {
+      editor.Right();
+    }
+  }
+
+}
+
 int main() {
   TestRunner tr;
+  RUN_TEST(tr, TestDefault);
   RUN_TEST(tr, TestEditing);
   RUN_TEST(tr, TestReverse);
   RUN_TEST(tr, TestNoText);
   RUN_TEST(tr, TestEmptyBuffer);
+
+  StressTestInsertGet();
+  StressTestCutPaste();
+  StressTestCut();
+  StressTestCopy();
+  StressTestLeftRight();
+
   return 0;
 }
